7296b91345bdce55f062f78872d9c862458fb5e0
Wai Yan <tin@cooper.edu>
2019-05-09 21:34:21 -0400

    test

--- sankeycsv.py
+++ sankeycsv.py
@@ -17,7 +17,7 @@ from dash.dependencies import Output, Input, State
 #     energy_data = json.loads(response.read())
  # moved inside function
 
-
+# Old town road
 #Employing dash to create a flask app. 
 app = dash.Dash(__name__)
 server = app.server
